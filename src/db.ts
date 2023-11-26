54d273918d9a677d24262e9f08a7f4867b8cf892
412 <laybacksound96@gmail.com>
2023-11-26 14:40:42 +0900

    database setup

---
+++ src/db.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@checksheet-0.epabadl.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(uri);
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB");
+const handleError = (error: any) => console.log(`Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import cors from "cors";
+import "./db";
 
 dotenv.config();
 const app = express();
 
+if (process.env.MODE === "dev") {
+  app.use(cors());
+  app.use(morgan("dev"));
+}
+
 app.use(bodyParser.json());
 
-// app.get("/", (req: Request, res: Response) => {
-//   res.send("hello, world!");
-// });
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello, world!");
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
